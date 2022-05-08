818ca8c79188ec13a97c3d044feb01d3da0535e2
Maity-123 <95854502+Maity-123@users.noreply.github.com>
2022-05-08 15:40:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var Recognition = new SpeechRecognition();
+
+function start()
+{
+    document.getElementsById("textbox").innerHTML = "";
+    Recognition.start();
+
+}
+
+Recognition.onresult = function(event) {
+    console.log(event);
+     
+    var Content = event.results[0][0].transcript;
+    console.log(Content);
+
+    document.getElementById("textbox").innerHTML = Content
+}
